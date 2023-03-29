a6b781cb93ea60610e9aa789829a13b2eaa4af7a
darshcloud <darshini.vn@gmail.com>
2023-03-29 16:30:42 -0700

    Android Data Storage UI changes

--- AndroidDataStorage/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/secondassig/androiddatastorage/R.java
+++ AndroidDataStorage/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/secondassig/androiddatastorage/R.java
@@ -8,67 +8,78 @@
 package com.secondassig.androiddatastorage;
 
 public final class R {
+  public static final class color {
+    public static final int colorAccent=0x7f010000;
+    public static final int colorPrimary=0x7f010001;
+    public static final int colorPrimaryDark=0x7f010002;
+    public static final int teal=0x7f010003;
+    public static final int white=0x7f010004;
+  }
   public static final class dimen {
-    public static final int activity_horizontal_margin=0x7f010000;
-    public static final int activity_vertical_margin=0x7f010001;
+    public static final int activity_horizontal_margin=0x7f020000;
+    public static final int activity_vertical_margin=0x7f020001;
   }
   public static final class drawable {
-    public static final int ic_launcher=0x7f020000;
+    public static final int ic_launcher=0x7f030000;
   }
   public static final class id {
-    public static final int action_settings=0x7f030000;
-    public static final int book_author=0x7f030001;
-    public static final int book_name=0x7f030002;
-    public static final int bookauthor_value=0x7f030003;
-    public static final int bookname_value=0x7f030004;
-    public static final int btn_close=0x7f030005;
-    public static final int cancel_btn=0x7f030006;
-    public static final int cancel_msg=0x7f030007;
-    public static final int description=0x7f030008;
-    public static final int description_value=0x7f030009;
-    public static final int enter_msg=0x7f03000a;
-    public static final int msg=0x7f03000b;
-    public static final int open_preferences=0x7f03000c;
-    public static final int open_sqlite=0x7f03000d;
-    public static final int save_btn=0x7f03000e;
-    public static final int save_msg=0x7f03000f;
-    public static final int saved_data=0x7f030010;
+    public static final int action_settings=0x7f040000;
+    public static final int book_author=0x7f040001;
+    public static final int book_name=0x7f040002;
+    public static final int bookauthor_value=0x7f040003;
+    public static final int bookname_value=0x7f040004;
+    public static final int btn_close=0x7f040005;
+    public static final int cancel_btn=0x7f040006;
+    public static final int cancel_msg=0x7f040007;
+    public static final int description=0x7f040008;
+    public static final int description_value=0x7f040009;
+    public static final int enter_msg=0x7f04000a;
+    public static final int headlineText=0x7f04000b;
+    public static final int msg=0x7f04000c;
+    public static final int open_preferences=0x7f04000d;
+    public static final int open_sqlite=0x7f04000e;
+    public static final int save_btn=0x7f04000f;
+    public static final int save_msg=0x7f040010;
+    public static final int saved_data=0x7f040011;
   }
   public static final class layout {
-    public static final int activity_main=0x7f040000;
-    public static final int activity_set_preferences=0x7f040001;
-    public static final int activity_sqlite=0x7f040002;
+    public static final int activity_main=0x7f050000;
+    public static final int activity_set_preferences=0x7f050001;
+    public static final int activity_sqlite=0x7f050002;
   }
   public static final class menu {
-    public static final int main=0x7f050000;
-    public static final int set_preferences=0x7f050001;
-    public static final int sqlite=0x7f050002;
+    public static final int main=0x7f060000;
+    public static final int set_preferences=0x7f060001;
+    public static final int sqlite=0x7f060002;
   }
   public static final class string {
-    public static final int action_settings=0x7f060000;
-    public static final int app_name=0x7f060001;
-    public static final int book_author=0x7f060002;
-    public static final int book_name=0x7f060003;
-    public static final int bookauthor_hint=0x7f060004;
-    public static final int bookname_hint=0x7f060005;
-    public static final int btn_close=0x7f060006;
-    public static final int cancel_btn=0x7f060007;
-    public static final int cancel_msg=0x7f060008;
-    public static final int description=0x7f060009;
-    public static final int description_hint=0x7f06000a;
-    public static final int enter_msg=0x7f06000b;
-    public static final int hello_world=0x7f06000c;
-    public static final int open_preferences=0x7f06000d;
-    public static final int open_sqlite=0x7f06000e;
-    public static final int sample_text=0x7f06000f;
-    public static final int save_btn=0x7f060010;
-    public static final int save_msg=0x7f060011;
-    public static final int save_success_msg=0x7f060012;
-    public static final int title_activity_set_preferences=0x7f060013;
-    public static final int title_activity_sqlite=0x7f060014;
+    public static final int action_settings=0x7f070000;
+    public static final int activity_text=0x7f070001;
+    public static final int app_name=0x7f070002;
+    public static final int book_author=0x7f070003;
+    public static final int book_name=0x7f070004;
+    public static final int bookauthor_hint=0x7f070005;
+    public static final int bookname_hint=0x7f070006;
+    public static final int btn_close=0x7f070007;
+    public static final int cancel_btn=0x7f070008;
+    public static final int cancel_msg=0x7f070009;
+    public static final int description=0x7f07000a;
+    public static final int description_hint=0x7f07000b;
+    public static final int enter_msg=0x7f07000c;
+    public static final int hello_world=0x7f07000d;
+    public static final int open_preferences=0x7f07000e;
+    public static final int open_sqlite=0x7f07000f;
+    public static final int preference_headline=0x7f070010;
+    public static final int sample_text=0x7f070011;
+    public static final int save_btn=0x7f070012;
+    public static final int save_msg=0x7f070013;
+    public static final int save_success_msg=0x7f070014;
+    public static final int sqllite_headline=0x7f070015;
+    public static final int title_activity_set_preferences=0x7f070016;
+    public static final int title_activity_sqlite=0x7f070017;
   }
   public static final class style {
-    public static final int AppBaseTheme=0x7f070000;
-    public static final int AppTheme=0x7f070001;
+    public static final int AppBaseTheme=0x7f080000;
+    public static final int AppTheme=0x7f080001;
   }
 }
